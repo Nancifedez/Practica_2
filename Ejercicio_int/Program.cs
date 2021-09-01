2f07c40078e4250eb48b35b62fa0d1f5a0ff5d5d
nanci faviola fernandez osuna <nancifedez-osuna@hotmail.com>
2021-08-31 19:16:12 -0700

    Add project files.

---
+++ Ejercicio_int/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero_1 = 5;
+            int numero_2 = 18;
+            int numero_3 = 20;
+            int suma = numero_1 + numero_2 + numero_3;
+            Console.WriteLine("El valor e suma es:" + suma);
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+
+         
+        }
+    }
+}
